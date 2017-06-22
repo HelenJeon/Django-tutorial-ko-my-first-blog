8af1f4ae8a7a10850a6c93bf3e381808c2c43a6e
jhjeon <jhjeon77@gmail.com>
2017-06-22 15:47:43 +0900

    css

--- mysite/settings.py
+++ mysite/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
 # LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'ko'
+LANGUAGE_CODE = 'ko-kr'
 
 # TIME_ZONE = 'UTC'
 TIME_ZONE = 'Asia/Seoul'
