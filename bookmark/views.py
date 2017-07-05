641d4fa1588611845bedc12805bbcd68928fcc87
jhjeon <jhjeon77@gmail.com>
2017-07-05 18:55:48 +0900

    bookmark url

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from bookmark.models import Bookmark # 테이블 조회를 위해 import
 
-# Create your views here.
+# ListView
+# ListView 제네릭 뷰를 상속
+# 명시적으로 지정하지 않아도 장고에서 지정해 주는 속성
+#   1) 컨텍스트 변수로 object_list를 사용
+#   2) 템플릿 파일을 모델명소문자_list.html 이름으로 지정
+class BookmarkLV(ListView):
+    model = Bookmark
+
+# DetailView
+# DetailView 제네릭 뷰를 상속
+# 명시적으로 지정하지 않아도 장고에서 지정해 주는 속성
+#   1) 컨텍스트 변수로 object를 사용
+#   2) 템플릿 파일을 모델명소문자_detail.html 이름으로 지정
+class BookmarkDV(DetailView):
+    model = Bookmark    
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.views.generic import ListView, DetailView
+from bookmark.models import Bookmark
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
-    #url(r'^bookmark', include('bookmark.urls'),)
+    url(r'^bookmark/$', ListView.as_view(model=Bookmark), name='index'),
+    url(r'^bookmark/(?P<pk>\d+)/$', DetailView.as_view(model=Bookmark), name='detail'),
 ]
