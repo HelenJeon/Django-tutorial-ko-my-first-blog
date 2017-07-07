1cef86477be04ac744160f4b479e8b81af4ae0ed
jhjeon <jhjeon77@gmail.com>
2017-07-07 16:34:51 +0900

    add app

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'modify_date')
+    list_filter = ('modify_date',)
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
--- bookmark/models.py
+++ bookmark/models.py
@@ -1,4 +1,7 @@
-from __future__ import unicode_literals # python 2.x 지원
+# 1.
+# 2. 객체를 문자열로 표현할 때 사용, title 반환
+
+from __future__ import unicode_literals # 1
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
@@ -7,7 +10,7 @@ class Bookmark(models.Model):
     title = models.CharField(max_length=100, blank=True, null=True)
     url = models.URLField('url', unique=True)
 
-    def __str__(self): # 객체를 문자열로 표현할 때 사용, title 반환
+    def __str__(self): # 2
         #return self.title
         return "%s %s" %(self.title, self.url)
 
