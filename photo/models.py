5ff03e3f63cda9429d05556cdcf3bdd64e2d3f3a
User <User@UserJ-MacBookPro.local>
2017-09-05 16:15:38 +0900

    clahge to save files

--- photo/models.py
+++ photo/models.py
@@ -35,7 +35,7 @@ class Album(models.Model):
 class Photo(models.Model):
     album = models.ForeignKey(Album) # 5
     title = models.CharField(max_length=50)
-    image = ThumbnailImageField(upload_to='photo/%Y/%m')
+    image = ThumbnailImageField(upload_to='media/photo/%Y/%m')
     description = models.TextField('Photo Description', blank=True)
     upload_date = models.DateTimeField('Upload Date', auto_now_add=True)
 
